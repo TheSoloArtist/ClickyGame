1df825b4802db9220e0d9ba541f62d593d065764
The Solo Artist <michael.solorzano@email.bakersfieldcollege.edu>
2020-09-24 16:16:04 -0700

    Updates

---
+++ precache-manifest.4df909eb7687be14d7a7468058729ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a860575cca0b080a0de61ae44a35ddca",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "110481c4e78490aa5bc8",
+    "url": "/ClickyGame/static/css/main.affca2df.chunk.css"
+  },
+  {
+    "revision": "9c4d0db947cc959a6c16",
+    "url": "/ClickyGame/static/js/2.15409e2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.15409e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110481c4e78490aa5bc8",
+    "url": "/ClickyGame/static/js/main.620186a3.chunk.js"
+  },
+  {
+    "revision": "29cfca7e2c10f6cba413",
+    "url": "/ClickyGame/static/js/runtime-main.5e5c4f3b.js"
+  }
+]);
